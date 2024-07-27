1bbf44339202fd4a8005244788dab6a169551a07
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-07-28 01:21:01 +0530

    flight model and all references added

--- src/info/info.js
+++ src/info/info.js
@@ -6,4 +6,6 @@ npx sequelize db:migrate
 npx sequelize model:generate --name Airport --attributes name:string,code:string,address:string,cityId:integer
 
 npx sequelize migration:generate --name update-city-airport-association
+
+npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer
 ```
