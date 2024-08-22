ea60fb14bd22893f0b134c181d04766ac2421856
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-08-22 14:06:01 +0530

    flight repo modified

--- src/repositories/flightRepository.js
+++ src/repositories/flightRepository.js
@@ -1,11 +1,11 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const CrudRepository = require('./crudRepository');
-const { Flight, Airplane, Airport, City } = require('../models');
-const db = require('../models');
-const { addRowLogOnFlights } = require('./queries');
+const CrudRepository = require("./crudRepository");
+const { Flight, Airplane, Airport, City } = require("../models");
+const db = require("../models");
+const { addRowLogOnFlights } = require("./queries");
 class FlightRepository extends CrudRepository {
-    constructor () {
+    constructor() {
         super(Flight);
     }
 
@@ -15,51 +15,57 @@ class FlightRepository extends CrudRepository {
             where: filter,
             order: sort,
             include: [
-            {
-                model: Airplane,
-                required: true,
-                as: 'airplaneDetail'
-            },
-            {
-                model: Airport,
-                required: true,
-                as: 'departureAirport',
-                on: {
-                    col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
-                },
-                include: {
-                    model: City,
+                {
+                    model: Airplane,
                     required: true,
-                }
-            },
-            {
-                model: Airport,
-                required: true,
-                as: 'arrivalAirport',
-                on: {
-                    col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    as: "airplaneDetail",
                 },
-                include: {
-                    model: City,
+                {
+                    model: Airport,
                     required: true,
-                }
-            }
-        ]
+                    as: "departureAirport",
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code")),
+                    },
+                    include: {
+                        model: City,
+                        required: true,
+                    },
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: "arrivalAirport",
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code")),
+                    },
+                    include: {
+                        model: City,
+                        required: true,
+                    },
+                },
+            ],
         });
-        
+
         return response;
     }
 
     async updateRemainingSeats(flightId, seats, dec = true) {
-        await db.sequelize.query(addRowLogOnFlights(flightId));
-        const flight = await Flight.findByPk(flightId);
-        if (+dec) {
-            await flight.decrement('totalSeats', {by: seats});
-            
-        } else {
-            await flight.increment('totalSeats', {by: seats});
-        };
-        return flight;
+        try {
+            const transaction = await db.sequelize.transaction();
+            await db.sequelize.query(addRowLogOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if (+dec) {
+                await flight.decrement("totalSeats", { by: seats }, { transaction: transaction });
+            } else {
+                await flight.increment("totalSeats", { by: seats }, { transaction: transaction });
+            }
+            await transaction.commit();
+            return flight;
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
+        }
     }
 }
 
